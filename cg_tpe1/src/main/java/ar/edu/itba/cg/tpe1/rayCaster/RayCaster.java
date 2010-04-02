aa2741feea5f43a0952d736a34d74ddb1f0716af
nachogoni <nachogoni@7ea94682-1f56-a2b9-6329-e4c1369357e3>
2010-04-02 23:18:06 +0000

    rayCaster

--- cg_tpe1/src/main/java/ar/edu/itba/cg/tpe1/rayCaster/RayCaster.java
+++ cg_tpe1/src/main/java/ar/edu/itba/cg/tpe1/rayCaster/RayCaster.java
@@ -1,8 +1,12 @@
 package ar.edu.itba.cg.tpe1.rayCaster;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import javax.vecmath.Point3d;
+
+import ar.edu.itba.cg.tpe1.geometry.Primitive;
+import ar.edu.itba.cg.tpe1.geometry.Ray;
 
 public class RayCaster {
 
@@ -20,22 +24,32 @@ public class RayCaster {
 	public BufferedImage getImage(int width, int height, int imageType) {
 
 		BufferedImage image = new BufferedImage(width, height, imageType);
-		
 		Point3d origin = camera.getOrigin();
+		Point3d intersection = null, aux = null;
+		Color color;
 		
-		// TODO Tirar rayos locos 
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
+				// Set infinite color
+				color = Color.BLACK;
+				
 				// Create a new Ray from camera, i, j
-				//Ray ray = new Ray(origin, i, j);
+				Ray ray = new Ray(origin, new Point3d());// TODO: (origin, i, j);
 				
 				// Find intersection in scene with ray
-				//for (Primitive p = )
+				for (Primitive p : scene.getList()) {
+					aux = p.intersect(ray);
+					if (aux != null && intersection != null &&
+						aux.distance(origin) < intersection.distance(origin)) {
+						intersection = aux;
+						color = p.getColor();
+					}
+				}
 				
 				// Set color to image(i, j)
+				image.setRGB(i, j, color.getRGB());
 			}
 		}
-		
 		
 		return image;
 	}
