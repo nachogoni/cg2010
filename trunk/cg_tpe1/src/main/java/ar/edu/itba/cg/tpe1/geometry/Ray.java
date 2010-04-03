1321d5021d7b1a9f925c85cfad625e85809e7ac7
nachogoni <nachogoni@7ea94682-1f56-a2b9-6329-e4c1369357e3>
2010-04-03 19:00:01 +0000

    Adding javadoc RayCasterThread optimization to a new class

--- trunk/cg_tpe1/src/main/java/ar/edu/itba/cg/tpe1/geometry/Ray.java
+++ trunk/cg_tpe1/src/main/java/ar/edu/itba/cg/tpe1/geometry/Ray.java
@@ -2,29 +2,59 @@ package ar.edu.itba.cg.tpe1.geometry;
 
 import javax.vecmath.Point3d;
 
+/**
+ * Ray representation
+ *
+ */
 public class Ray {
 	
 	private Point3d origin;
 	
 	private Point3d direction;
 	
+	/**
+	 * Constructor for Ray class
+	 * 
+	 * @param origin Position where from the ray starts 
+	 * @param direction Direction for the ray
+	 */
 	public Ray(Point3d origin, Point3d direction) {
 		this.origin = origin;
 		this.direction = direction;
 	}
 	
+	/**
+	 * Position where from the ray starts
+	 * 
+	 * @return Origin point
+	 */
 	public Point3d getOrigin() {
 		return origin;
 	}
 
+	/**
+	 * Set position where from the ray starts
+	 * 
+	 * @param origin New position
+	 */
 	public void setOrigin(Point3d origin) {
 		this.origin = origin;
 	}
 
+	/**
+	 * Get ray direction
+	 * 
+	 * @return Ray direction
+	 */
 	public Point3d getDirection() {
 		return direction;
 	}
 
+	/**
+	 * Set ray direction
+	 * 
+	 * @param direction New ray direction
+	 */
 	public void setDirection(Point3d direction) {
 		this.direction = direction;
 	}
---
+++ trunk/cg_tpe1/src/main/java/ar/edu/itba/cg/tpe1/rayCaster/RayCasterThread.java
@@ -0,0 +1,140 @@
+package ar.edu.itba.cg.tpe1.rayCaster;
+
+import java.awt.Color;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import javax.vecmath.Point3d;
+
+import ar.edu.itba.cg.tpe1.geometry.Primitive;
+import ar.edu.itba.cg.tpe1.geometry.Ray;
+
+/**
+ * RayCasterThread is an optimization using threads 
+ */
+class RayCasterThread extends Thread {
+
+	private Scene scene;
+	private Camera camera;
+	private int fromX;
+	private int toX;
+	private int fromY;
+	private int toY;
+	private RayCaster rayCaster;
+	private CyclicBarrier cb;
+
+	/**
+	 * Constructor for RayCasterThread class
+	 * 
+	 * @param scene Scene representation to work with
+	 * @param camera Actual camera where the viewer is 
+	 * @param rayCaster RayCaster class parent
+	 * @param cb CyclicBarrier for the threads
+	 */
+	public RayCasterThread(Scene scene, Camera camera, RayCaster rayCaster, CyclicBarrier cb) {
+		this.cb = cb;
+		this.scene = scene;
+		this.camera = camera;
+		this.rayCaster = rayCaster;
+	}
+
+	/**
+	 * Set image portion to work with
+	 * 
+	 * @param fromX First column
+	 * @param toX Last column
+	 * @param fromY First row
+	 * @param toY Last row
+	 */
+	public void setPortion(int fromX, int toX, int fromY, int toY) {
+		this.fromX = fromX;
+		this.toX = toX;
+		this.fromY = fromY;
+		this.toY = toY;
+	}
+
+	/**
+	 * Get last column of the image to work with
+	 * 
+	 * @return last column
+	 */
+	public int getToX() {
+		return toX;
+	}
+	
+	
+	/**
+	 * Get first column of the image to work with
+	 * 
+	 * @return first column
+	 */
+	public int getFromX() {
+		return fromX;
+	}
+
+	/**
+	 * Get first row of the image to work with
+	 * 
+	 * @return first row
+	 */
+	public int getFromY() {
+		return fromY;
+	}
+	
+	/**
+	 * Get last row of the image to work with
+	 * 
+	 * @return last row
+	 */
+	public int getToY() {
+		return toY;
+	}
+	
+	/**
+	 * Start the rayCaster
+	 * 
+	 */
+	public void run() {
+		while ( true ){
+			System.out.println(cb.getNumberWaiting()+" threads are waiting");
+			try { cb.await(); } catch (InterruptedException e) { 
+				//System.out.println("Someone told me to die.");
+				return;
+			} catch (BrokenBarrierException e) { 
+				//System.out.println("Someone broke the barrier.");
+				return;
+			}
+
+			Point3d origin = camera.getOrigin();
+			Point3d intersection = null, aux = null;
+			Color color;
+			for (int i = fromX; i < toX; i++) {
+				for (int j = fromY; j < toY; j++) {
+					// Set infinite color
+					color = Color.BLUE;
+					
+					// Create a new Ray from camera, i, j
+					Ray ray = new Ray(origin, new Point3d());// TODO: (origin, i, j);
+					
+					// Find intersection in scene with ray
+					for (Primitive p : scene.getList()) {
+						aux = p.intersect(ray);
+						if (aux != null && (intersection == null || (intersection != null &&
+							aux.distance(origin) < intersection.distance(origin)))) {
+							intersection = aux;
+							color = p.getColor();
+						}
+					}
+						
+					// Set color to image(i, j)
+					synchronized (rayCaster.image) {
+						rayCaster.getImage().setRGB(i, j, color.getRGB());
+					}
+				}
+			}
+
+			//System.out.println(cb.getNumberWaiting()+" threads are waiting");
+			try { cb.await(); } catch (InterruptedException e) { e.printStackTrace(); } catch (BrokenBarrierException e) { e.printStackTrace(); }
+		}
+	}
+}
