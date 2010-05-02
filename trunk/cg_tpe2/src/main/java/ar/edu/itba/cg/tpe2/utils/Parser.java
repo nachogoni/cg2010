1ce07e043e11d5044dac573ddee7816696e44b6e
juantenaillon <juantenaillon@7ea94682-1f56-a2b9-6329-e4c1369357e3>
2010-05-02 00:54:44 +0000

    Faltan los Object y los Transform.  No le den bola al archivo ParserTest. Está hecho para que pueda probar el parser! Abrazo de gol!

--- trunk/cg_tpe2/src/main/java/ar/edu/itba/cg/tpe2/utils/Parser.java
+++ trunk/cg_tpe2/src/main/java/ar/edu/itba/cg/tpe2/utils/Parser.java
@@ -1,6 +1,5 @@
 package ar.edu.itba.cg.tpe2.utils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.vecmath.Point2d;
@@ -34,6 +33,10 @@ public class Parser {
 		aParser = new FileParser(this.filename);
 		while(true){
 			current = aParser.getNextToken();
+			System.out.println(current);
+			if(current == null){
+				return null;
+			}
 			if(current.equalsIgnoreCase("image")){
 				System.out.println("Reading the image settings...");
 				this.parseImageSettings();
@@ -43,13 +46,137 @@ public class Parser {
 			} else if (current.equalsIgnoreCase("shader")){
 				System.out.println("Reading Shader settings...");
 				this.parseShaderSettings();
+			} else if (current.equalsIgnoreCase("light")){
+				System.out.println("Reading Light Settings...");
+				this.parseLightSettings();
 			}
 		}
 	}
 
+	
 
-	private void parseShaderSettings() {
-		// TODO Auto-generated method stub
+	private void parseLightSettings() throws IOException {
+		String current, type, light_color;
+		Double __a, __b, __c, power;
+		Point3d light_pos;
+		do{
+			current = aParser.getNextToken();
+			if(current.equals("type")){
+				type = aParser.getNextToken();
+			} else if (current.equals("color")){
+				// We discard the first {
+				current = aParser.getNextToken();
+				current = aParser.getNextToken() + aParser.getNextToken();
+				int length = current.length();
+				light_color = current.substring(1, length);
+				__a = new Double(aParser.getNextToken());
+				__b = new Double(aParser.getNextToken());
+				__c = new Double(aParser.getNextToken());
+				// Now we discard the last }
+				current = aParser.getNextToken();
+				current = light_color;
+			}else if (current.equals("power")){
+				power = new Double(aParser.getNextToken());
+			}else if (current.equals("p")){
+				light_pos = new Point3d(new Double(aParser.getNextToken()),
+						new Double(aParser.getNextToken()),
+						new Double(aParser.getNextToken()));
+			}
+		}while(!current.equals("}"));
+	}
+
+
+	private void parseShaderSettings() throws IOException {
+		String current;
+		String name, type, texture_path;
+		String shader_color, abs_color;
+		Double __a, __b, __c, eta, abs_dst, abs__a, abs__b, abs__c;
+		Integer samples;
+		do{
+			current = aParser.getNextToken();
+			if(current.equals("name")){
+				name = aParser.getNextToken();
+			} else if (current.equals("type")){
+				type = aParser.getNextToken();
+				// Depending on the type of Shader...
+				if(type.equals("phong")){
+					do{
+						current = aParser.getNextToken();
+						if(current.equals("texture")){
+							texture_path = aParser.getNextToken();
+							int length = texture_path.length();
+							texture_path = texture_path.substring(1, length);
+						} else if(current.equals("spec")){
+							// We discard the first {
+							current = aParser.getNextToken();
+							current = aParser.getNextToken() + aParser.getNextToken();
+							int length = current.length();
+							shader_color = current.substring(1, length);
+							__a = new Double(aParser.getNextToken());
+							__b = new Double(aParser.getNextToken());
+							__c = new Double(aParser.getNextToken());
+							// Now we discard the last }
+							current = aParser.getNextToken();
+							current = type;
+						} else if (current.equals("samples")){
+							samples = new Integer(aParser.getNextToken());
+						}
+					}while(!current.equals("}"));
+				} else if (type.equals("glass")){
+					do{
+						current = aParser.getNextToken();
+						if(current.equals("eta")){
+							eta = new Double(aParser.getNextToken());
+						} else if(current.equals("color")){
+							// We discard the first {
+							current = aParser.getNextToken();
+							current = aParser.getNextToken() + aParser.getNextToken();
+							int length = current.length();
+							shader_color = current.substring(1, length);
+							__a = new Double(aParser.getNextToken());
+							__b = new Double(aParser.getNextToken());
+							__c = new Double(aParser.getNextToken());
+							// Now we discard the last }
+							current = aParser.getNextToken();
+							current = type;
+						} else if (current.equals("absorbtion.distance")){
+							abs_dst = new Double(aParser.getNextToken());
+						} else if (current.equals("absorbtion.color")){
+							// We discard the first {
+							current = aParser.getNextToken();
+							current = aParser.getNextToken() + aParser.getNextToken();
+							int length = current.length();
+							abs_color = current.substring(1, length);
+							abs__a = new Double(aParser.getNextToken());
+							abs__b = new Double(aParser.getNextToken());
+							abs__c = new Double(aParser.getNextToken());
+							// Now we discard the last }
+							current = aParser.getNextToken();
+							current = type;
+						}
+					}while(!current.equals("}"));
+				} else if (type.equals("mirror")){
+					do{
+						current = aParser.getNextToken();
+						if(current.equals("refl")){
+							// We discard the first {
+							current = aParser.getNextToken();
+							current = aParser.getNextToken() + aParser.getNextToken();
+							int length = current.length();
+							shader_color = current.substring(1, length);
+							__a = new Double(aParser.getNextToken());
+							__b = new Double(aParser.getNextToken());
+							__c = new Double(aParser.getNextToken());
+							// Now we discard the last }
+							current = aParser.getNextToken();
+							current = type;
+						}
+					}while(current.equals("}"));
+				}
+			}
+		}while(!current.equals("}"));
+		
+		// Aca habria que armar el objeto
 		
 	}
 
@@ -123,6 +250,7 @@ public class Parser {
 		//Aca habria que crear el objeto.
 		
 	}
+	
 	
 	
 	
