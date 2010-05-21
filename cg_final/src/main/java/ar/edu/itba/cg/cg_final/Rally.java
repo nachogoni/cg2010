dfaccc6295864e1136549af8cc6474b7b9e89db2
nachogoni <nachogoni@7ea94682-1f56-a2b9-6329-e4c1369357e3>
2010-05-21 00:52:24 +0000

    Primer aproach con JME2

---
+++ cg_final/src/main/java/ar/edu/itba/cg/cg_final/Rally.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.cg.cg_final;
+
+import com.jme.app.SimpleGame;
+import com.jme.bounding.BoundingBox;
+import com.jme.math.Vector3f;
+import com.jme.scene.shape.Sphere;
+
+public class Rally extends SimpleGame {
+
+	public static void main(String[] args) {
+		Rally rally = new Rally();
+		rally.setConfigShowMode(ConfigShowMode.AlwaysShow);
+		rally.start();
+	}
+
+	protected void simpleInitGame() {
+		display.setTitle("Tutorial 1");
+		Sphere s = new Sphere("Sphere", 30, 30, 25);
+		s.setLocalTranslation(new Vector3f(0, 0, -40));
+		s.setModelBound(new BoundingBox());
+		s.updateModelBound();
+		rootNode.attachChild(s);
+	}
+}
